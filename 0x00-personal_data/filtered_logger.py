f72b3b7a1d91127e006d293b1d79e41bbd787717
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-03 22:18:24 +0100

    0x00 task 2 - updated PII fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import logging
 import re
 
 
-PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
