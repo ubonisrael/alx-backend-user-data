85398ce275f6d49d92d029700342551c8aad7838
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-08 21:43:07 +0100

    0x01 tasks 10 initial attempt

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Auth Class Module"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -49,3 +51,28 @@ class BasicAuth(Auth):
         user_email = decoded_base64_authorization_header.split(':')[0]
         user_passwd = decoded_base64_authorization_header.split(':')[1]
         return user_email, user_passwd
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password."""
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        users = User.search({'email': user_email})
+        if len(users) == 0:
+            return None
+        user = users[0]
+        if user is None:
+            return None
+        if user.is_valid_password(user_pwd) is False:
+            return None
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves the User instance for a request"""
+        auth_h = self.authorization_header(request)
+        b64_auth_h = self.extract_base64_authorization_header(auth_h)
+        decoded_header = self.decode_base64_authorization_header(b64_auth_h)
+        user_cred = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user_cred[0], user_cred[1])
