32079661051ffe2712dde13a980f58141c114b66
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-10 22:00:12 +0100

    0x02 task 2 initial attempt

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,17 @@ from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """provides session auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session id for a user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
