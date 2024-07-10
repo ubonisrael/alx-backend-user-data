7195927af5e9d98b838407eabeea8d14eed73c83
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-10 21:52:37 +0100

    0x02 task 1 initial attempt

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,8 +17,11 @@ auth = os.getenv('AUTH_TYPE')
 if auth:
     from api.v1.auth.auth import Auth
     from api.v1.auth.basic_auth import BasicAuth
+    from api.v1.auth.session_auth import SessionAuth
     if auth == 'basic_auth':
         auth = BasicAuth()
+    elif auth == 'session_auths':
+        auth = SessionAuth()
     else:
         auth = Auth()
 
