1511af68e050d6244b92139528a9cd1cc73d3ff8
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-11 20:34:45 +0100

    0x02 task 8 initial attempt

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,14 @@ def session_login() -> str:
             response.set_cookie(os.getenv('SESSION_NAME'), session_id)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def session_logout() -> str:
+    """ logs out a user
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    return False, abort(404)
