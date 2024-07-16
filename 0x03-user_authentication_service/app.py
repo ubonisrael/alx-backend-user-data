70ff14f08499d638551319e97730e9d2032c9213
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-16 12:57:44 +0100

    0x03 fixed lint errors for app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,7 +11,7 @@ AUTH = Auth()
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """returns homepage"""
-    return jsonify({"message":"Bienvenue"})
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
@@ -20,9 +20,11 @@ def user_signup():
     payload = dict(request.form)
     try:
         AUTH.register_user(payload['email'], payload['password'])
-        return jsonify({"email": f"{payload['email']}", "message": "user created"})
+        return jsonify({"email": f"{payload['email']}",
+                        "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
