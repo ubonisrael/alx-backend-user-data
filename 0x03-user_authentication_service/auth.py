2486d9ef68e4179932cc53b82c08368c5a7458f7
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-16 13:55:50 +0100

    0x03 initial attempts for task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,6 +74,16 @@ class Auth:
         except NoResultFound:
             raise ValueError()
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates a user's password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hp = _hash_password(password)
+            self._db.update_user(user.id, reset_token=None,
+                                 hashed_password=hp)
+        except NoResultFound:
+            raise ValueError()
+
 
 def _hash_password(password: str) -> bytes:
     """takes in a password and returns bytes"""
